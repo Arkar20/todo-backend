89ff7b4c6a24437b7fb9b558dabead585ea89b62
arkar20 <arkar20011@gmail.com>
2025-01-23 17:14:51 -0800

    crud

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,10 +1,58 @@
 import { Request, Response } from "express";
 import prisma from "../database";
+import { Todo } from "@prisma/client";
 
 export const TodoController = {
-    findAll: async (req: Request, res: Response): Promise<any> => {
+    findAll: async (req: Request, res: Response) => {
         const todos = await prisma.todo.findMany();
 
-        return res.json(todos);
+        res.json(todos);
+    },
+
+    findById: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        const todoId = parseInt(id);
+
+        const todos = await prisma.todo.findUnique({
+            where: { id: todoId },
+        });
+
+        res.json(todos);
+    },
+
+    create: async (req: Request, res: Response) => {
+        const data = req.body;
+
+        const todo = await prisma.todo.create(data);
+
+        res.json(todo);
+    },
+
+    update: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        const todoId = parseInt(id);
+
+        const { data } = req.body;
+
+        const todo = prisma.todo.update({
+            where: { id: todoId },
+            data,
+        });
+
+        res.json(todo);
+    },
+
+    delete: async (req: Request, res: Response) => {
+        const { id } = req.params;
+
+        const todoId = parseInt(id);
+
+        const todo = await prisma.todo.delete({
+            where: { id: todoId },
+        });
+
+        res.json(todo);
     },
 };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,7 @@ import { TodoController } from "../controllers";
 const router = Router();
 
 router.get("/", TodoController.findAll);
+router.post("/", TodoController.create);
+router.get("/:id", TodoController.findById);
 
 export default router;
